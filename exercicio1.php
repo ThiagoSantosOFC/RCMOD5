96882a586f19573766a69278e8f3a2a5321f1061
Thiago <74683757+ThiagoSantosOFC@users.noreply.github.com>
2022-07-04 17:05:11 +0100

    Create exercicio1.php

---
+++ exercicio1.php
@@ -0,0 +1,17 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+    formulario de login
+    <form action="exercicio1.php" method="post">
+        Nome:<input type="text" name="nome" placeholder="nome">
+        <br>
+        <input type="submit" value="Submeter">
+        <input type="reset" name"limpar"> Limpar  
+    </form>
+</body>
+</html>
